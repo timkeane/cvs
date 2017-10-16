050fd10db7b87ce24795af1e0afe269a0a0b190c
timkeane <tkeane@doitt.nyc.gov>
2017-10-16 15:34:42 -0400

    another styling option

--- src/main/webapp/js/style.js
+++ src/main/webapp/js/style.js
@@ -1,21 +1,38 @@
 var finderStyle = function(feature, resolution){
   var zoom = nyc.ol.TILE_GRID.getZForResolution(resolution);
-  var idx = feature.locationIdx;
+  var count = feature.getCountAtLocation();
   finderStyle.cache[zoom] = finderStyle.cache[zoom] || {};
-  if (!finderStyle.cache[zoom][idx]){
+  if (!finderStyle.cache[zoom][count]){
     var scale = 12;
     if (zoom > 11) scale = 16;
     if (zoom > 14) scale = 24;
     if (zoom > 17) scale = 32;
-    finderStyle.cache[zoom][idx] = new ol.style.Style({
-      image: new ol.style.Icon({
-        src: 'img/icon.svg',
-        anchor: [.5 + (-.12 * idx), .5 + (.12 * idx)],
-        scale: scale / 24
+    var style = [
+      new ol.style.Style({
+        image: new ol.style.Icon({
+          src: 'img/' + (count == 1 ? 'icon.svg' : 'stack.svg'),
+          scale: scale / 33
+        })
       })
-    });
+    ];
+    if (zoom > 13 && count > 1) {
+      style.push(
+        new ol.style.Style({
+          text: new ol.style.Text({
+            fill: new ol.style.Fill({color: '#fff'}),
+            font: '20px sans-serif',
+            text: count + '',
+            offsetX: 5 * scale / 33,
+            offsetY: -4 * scale / 33,
+            textAlign: 'center',
+            scale: scale / 33
+          })
+        })
+      );
+    }
+    finderStyle.cache[zoom][count] = style;
   }
-  return finderStyle.cache[zoom][idx];
+  return finderStyle.cache[zoom][count];
 };
 
 finderStyle.cache = {};
