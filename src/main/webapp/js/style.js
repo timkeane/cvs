c302d0ccfb8375b0301fcf5b023a26994282fd46
timkeane <tkeane@doitt.nyc.gov>
2017-10-17 14:15:57 -0400

    imgSize for IE

--- src/main/webapp/js/style.js
+++ src/main/webapp/js/style.js
@@ -12,7 +12,8 @@ var finderStyle = function(feature, resolution){
       new ol.style.Style({
         image: new ol.style.Icon({
           src: 'img/' + (count == 1 ? 'icon.svg' : 'stack.svg'),
-          scale: size / 33
+          scale: size / 33,
+          imgSize: [33, 33]
         })
       })
     ];
